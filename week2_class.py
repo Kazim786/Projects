a73ffc0f237308d7b14b3c93f334b2c13d073398
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-01-21 11:07:55 -0600

    Creating classes and objects of classes

--- week2_class.py
+++ week2_class.py
@@ -1,10 +1,13 @@
 class student:
+    def __init__ (self):
+        print("constructor called") #Everytime object is created, Constructor is called. 
+        # it prints "constructor called 3 times b/c you have made 3 objects"
     def good_morning(self):
         print("Good morning ")
 Alina = student()
 Sean = student()
 Alex = student()
 
-Alex.good_morning()
+# Alex.good_morning()
 
 # This is creating a class and the objects of that class. In this example I also called the object with its method.
