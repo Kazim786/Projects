31e995213648dd21b78904e58c48e0bc0810dc00
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-01-21 10:57:39 -0600

    Tuesday week 2 notes

---
+++ week2_class.py
@@ -0,0 +1,10 @@
+class student:
+    def good_morning(self):
+        print("Good morning ")
+Alina = student()
+Sean = student()
+Alex = student()
+
+Alex.good_morning()
+
+# This is creating a class and the objects of that class. In this example I also called the object with its method.
