6b68ca306765fab4b22870db368976c31988f907
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-01-31 09:39:43 -0600

    squared the values

--- algorithm_number2.py
+++ algorithm_number2.py
@@ -3,8 +3,11 @@ a=0
 b=0
 c=0
 for i in range(1,1000): #max value for any side of the triangle must be less      than the semiperimeter
+    
     for j in range(i,499):
+        
         for k in range(j,499):
+            
             if(i+j+k==1000 and i**2+j**2==k**2):
                 a=i
                 b=j
@@ -14,7 +17,9 @@ print(a)
 print(b)
 print(c)
 print(a*b*c)
-
+print(math.sqrt(a))
+print(math.sqrt(b))
+print(math.sqrt(c))
 
 
 
