f1ae9bd93b2d33e5c2d6a0755c4eeb0d8c4efac3
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-01-31 11:19:46 -0600

    Eric helped

--- algorithm_number2.py
+++ algorithm_number2.py
@@ -26,21 +26,46 @@
 #     def __init__(self):
 #         pass
         
-def unique_characters():
-    new_list=[]
 
+        #Erics Code:
+def unique_characters():
     aList1= ["a", "b", "c", "d", "e"]
     aList2= ["a", "b", "c", "d", "f"]
-    for letter1 in aList1:
-        for letter2 in aList2:
-            if(letter1 == letter2):
-                new_list.append(letter1)
-                print(new_list)
-            else:
-                print("does not match")
+    new_list=[]
+    for i in aList1:
+        if i in aList2:
+            pass
+        else:
+            new_list.append(i)
+    for j in aList2:
+        pass
+    else:
+        new_list.append(j)
     print(new_list)
 
 unique_characters()
+
+
+
+#This is my code.
+#     aList1= ["a", "b", "c", "d", "e"]
+#     aList2= ["a", "b", "c", "d", "f"]
+#     for letter1 in range (len(aList1)):
+#         print(letter1)
+#         for letter2 in range (len(aList2)):
+#             print(letter2)
+#             if(aList1[letter1] == aList2[letter2]):
+#                 del aList1[letter1]
+#                 del aList2[letter2]
+#                 break
+#                 # new_list.append(letter1)
+#                 # new_list.append(letter2)
+#                 # print(new_list)
+            
+#     print(aList1)
+#     print(aList2)
+
+
             
 
 
