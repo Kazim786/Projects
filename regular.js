ab44f9a754ba7d6e64589dd166e2b5a48798af41
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-02-13 09:41:41 -0600

    js and jquery

--- regular.js
+++ regular.js
@@ -4,7 +4,8 @@ $(()=>{
     // let result = $('div')
     // console.log(result)
     // $('#paragraph') //to get ID 
-    // result = $(".myImage")
+    // let $result = $(".myImage")
+    // always add $ even to your variables so you know what is JS and what is JQuery
     $('div').html("<span style= 'background-color: yellow; font-size:30px'>This is a great day</span>") 
     //^^ This is adding html within the tags. So basically adding tags within the div.
 
