fc96b4d42f4dec0fd6793cdc726836b9cfeaf13b
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-02-13 09:40:17 -0600

    doing more

--- regular.js
+++ regular.js
@@ -1,9 +1,12 @@
 
 
 $(()=>{
-    let result = $('div')
-    console.log(result)
-    $('#paragraph') //to get ID 
+    // let result = $('div')
+    // console.log(result)
+    // $('#paragraph') //to get ID 
+    // result = $(".myImage")
+    $('div').html("<span style= 'background-color: yellow; font-size:30px'>This is a great day</span>") 
+    //^^ This is adding html within the tags. So basically adding tags within the div.
 
 })
 
